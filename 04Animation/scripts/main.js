553a0108844bd943b5f9e12d3b49219554941048
Jacob <jacobvaandrager@me.com>
2021-05-11 14:33:59 +0200

    4 done

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,12 +1,21 @@
 
 
-// // changing individual properties with code and using setInterval
-// var rotationSpeed = 0.01;
-// var myOtherBox = document.getElementById('myOtherBox');
+// changing individual properties with code and using setInterval
+var rotationSpeed = -0.01;
+var myOtherBox = document.getElementById('myOtherBox');
 
-// function spin(){
-// 	myOtherBox.object3D.rotation.x += rotationSpeed;
-// 	console.log(myOtherBox.object3D.rotation.x);
-// }
+function spin(){
+    rotationSpeed = rotationSpeed + 0.01
 
-// setInterval(spin, 16); //equivalent to 60 fps
+    if (rotationSpeed >= 0.2) {
+
+        rotationSpeed = 0.01
+
+    }
+    myOtherBox.object3D.rotation.x += rotationSpeed;
+    myOtherBox.object3D.rotation.y += rotationSpeed;
+    console.log(rotationSpeed);
+    
+}
+
+setInterval(spin, 16); //equivalent to 60 fps
